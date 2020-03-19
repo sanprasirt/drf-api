9d1325b892b20c9f398938856b587ca8c5831aad
Sanprasirt <kuforce@gmail.com>
2020-03-19 21:55:31 +0700

    update authentication

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # third party
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,6 +11,9 @@ from .models import Post
 
 
 class TestView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+    
     def get(self, request, *args, **kwargs):
         qs = Post.objects.all()
         # post = qs.first()
