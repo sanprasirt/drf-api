a1dca62f72cc467b5ad05df33c742287992b9b03
Sanprasirt boonma <kuforce@gmail.com>
2020-03-19 18:18:21 +0700

    first commit

---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# third party
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name': 'jonh',
+            'age': 23
+        }
+        return Response(data=data)
+
+# Create your views here.
+# def test_view(request):
+#     data = {
+#         'name': 'jonh',
+#         'age': 23
+#     }
+#     return JsonResponse(data) 
